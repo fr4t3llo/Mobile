1e6cc29f422ef029e222e6b8518909f27075d312
Saifeddine Kasmi <skasmi@e3r8p4.1337.ma>
2024-06-27 20:20:08 +0100

    update and finish ex00

--- mobileModule00/ex00/lib/main.dart
+++ mobileModule00/ex00/lib/main.dart
@@ -5,10 +5,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   void clickMe() {
-    print("button Pressed");
+    debugPrint("Button pressed");
   }
 
   @override
@@ -19,23 +19,46 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           body: Stack(
             children: [
-              Column(
-                children: [
-                  const Expanded(
-                    child: Center(
-                      child: Text(
-                        'a simple text',
-                        style: TextStyle(color: Colors.amber),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 121, 163, 5),
+                          borderRadius: BorderRadiusDirectional.circular(10)),
+                      // color: Colors.amber,
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          'A simple text',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontSize: 20,
+                            fontFamily: 'my',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: clickMe,
-                    child: const Text(
-                      'Click me',
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 177, 4, 70),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      onPressed: clickMe,
+                      child: const Text(
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'my',
+                        ),
+                        'Click me',
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ],
           ),
