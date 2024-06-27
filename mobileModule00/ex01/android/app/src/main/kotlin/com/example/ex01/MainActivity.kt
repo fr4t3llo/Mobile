ec488ecdff13e01a849eaf5c73eeac38684e8e91
Saifeddine Kasmi <skasmi@e3r8p4.1337.ma>
2024-06-27 20:34:40 +0100

    ex01 init

---
+++ mobileModule00/ex01/android/app/src/main/kotlin/com/example/ex01/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ex01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mobileModule00/ex01/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+const String mytext = 'A simple text';
+const String mysecondtext = 'hello world!';
+
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+
+
+
+
+
+
+  
+  void clickMe() {
+    debugPrint("Button pressed");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Stack(
+            children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 121, 163, 5),
+                          borderRadius: BorderRadiusDirectional.circular(10)),
+                      // color: Colors.amber,
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          mytext,
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontSize: 20,
+                            fontFamily: 'my',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 177, 4, 70),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      onPressed: clickMe,
+                      child: const Text(
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'my',
+                        ),
+                        'Click me',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
