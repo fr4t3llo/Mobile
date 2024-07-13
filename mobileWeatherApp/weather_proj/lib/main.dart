7868fce13416557ce52a97ee93bcfeb60909d9a9
Saifeddine Kasmi <skasmi@e3r7p12.1337.ma>
2024-07-13 18:59:22 +0100

    update by FRATELLO

--- mobileWeatherApp/weather_proj/lib/main.dart
+++ mobileWeatherApp/weather_proj/lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:device_preview_plus/device_preview_plus.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import "package:iconsax/iconsax.dart";
+// Ensure you have the correct package for icons
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true, // Enable DevicePreview if necessary
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +19,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(),),
-    );
+        builder:
+            DevicePreview.appBuilder, // Ensure DevicePreview is used properly
+        home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.white,
+              title: Container(
+                height: 40,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(25)),
+                // title: const CupertinoSearchTextField(),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: const Icon(Icons.search_rounded),
+                        ),
+                        Text(
+                          'search',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: const Color.fromARGB(255, 88, 88, 88),
+                            fontFamily: 'my',
+                          ),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Container(
+                        width: 70,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            // border: Border.all(),
+                            borderRadius: BorderRadius.circular(25)),
+                        child: Center(
+                            child: Text(
+                          'Go',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontFamily: 'my',
+                              fontWeight: FontWeight.bold),
+                        )),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
