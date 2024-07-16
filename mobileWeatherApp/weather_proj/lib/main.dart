96deed1cfa36dfc67f1ccee433197a1869fd3294
Saifeddine Kasmi <skasmi@e3r10p14.1337.ma>
2024-07-16 13:12:01 +0100

    update by FRATELLO

--- mobileWeatherApp/weather_proj/lib/main.dart
+++ mobileWeatherApp/weather_proj/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:device_preview_plus/device_preview_plus.dart';
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'searchpage.dart';
 import "package:iconsax/iconsax.dart";
+import 'package:weather_proj/searchpage.dart';
 // Ensure you have the correct package for icons
 
 void main() {
@@ -19,6 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         builder:
             DevicePreview.appBuilder, // Ensure DevicePreview is used properly
         home: SafeArea(
@@ -26,51 +29,62 @@ class MyApp extends StatelessWidget {
             appBar: AppBar(
               elevation: 0,
               backgroundColor: Colors.white,
-              title: Container(
-                height: 40,
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(25)),
-                // title: const CupertinoSearchTextField(),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: const Icon(Icons.search_rounded),
-                        ),
-                        Text(
-                          'search',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: const Color.fromARGB(255, 88, 88, 88),
-                            fontFamily: 'my',
+              title: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SearchPage()),
+                  );
+                },
+                child: Container(
+                  height: 40,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Icon(Iconsax.search_favorite),
                           ),
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(3.0),
-                      child: Container(
-                        width: 70,
-                        height: 30,
-                        decoration: BoxDecoration(
-                            color: Colors.blue,
-                            // border: Border.all(),
-                            borderRadius: BorderRadius.circular(25)),
-                        child: Center(
-                            child: Text(
-                          'Go',
-                          style: TextStyle(
+                          Text(
+                            'search',
+                            style: TextStyle(
                               fontSize: 15,
+                              color: Color.fromARGB(255, 88, 88, 88),
                               fontFamily: 'my',
-                              fontWeight: FontWeight.bold),
-                        )),
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: InkWell(
+                          onTap: () {
+                            debugPrint("hello");
+                          },
+                          child: Container(
+                            width: 70,
+                            height: 30,
+                            decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(25)),
+                            child: const Center(
+                                child: Text(
+                              'Go',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontFamily: 'my',
+                                  fontWeight: FontWeight.bold),
+                            )),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
---
+++ mobileWeatherApp/weather_proj/lib/searchpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(),
+    ));
+  }
+}
