c6e952b85fc4c8ae13272e8546d6e93568546484
Saifeddine Kasmi <skasmi@e3r3p13.1337.ma>
2024-07-17 12:27:06 +0100

    update by FRATELLO

--- mobileWeatherApp/weather_proj/lib/main.dart
+++ mobileWeatherApp/weather_proj/lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:device_preview_plus/device_preview_plus.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:iconsax/iconsax.dart';
+// Ensure you have the correct package for icons
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true, // Enable DevicePreview if necessary
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -15,111 +23,119 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int _index = 0;
-  TextEditingController textController = TextEditingController();
-  List<String> centerText = [
-    'Currently',
-    'Today',
-    'Weekly',
+  String location = '';
+  TextEditingController text1 = TextEditingController();
+  List<Widget> content = [
+    const Text(
+      'Currently',
+      style: TextStyle(
+        fontFamily: 'my',
+        fontWeight: FontWeight.bold,
+        fontSize: 25,
+      ),
+    ),
+    const Text(
+      'Today',
+      style: TextStyle(
+        fontFamily: 'my',
+        fontWeight: FontWeight.bold,
+        fontSize: 25,
+      ),
+    ),
+    const Text(
+      'Weekly',
+      style: TextStyle(
+        fontFamily: 'my',
+        fontWeight: FontWeight.bold,
+        fontSize: 25,
+      ),
+    ),
+    Text(
+      '',
+      style: TextStyle(
+        fontFamily: 'my',
+        fontWeight: FontWeight.bold,
+        fontSize: 25,
+      ),
+    ),
   ];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  centerText[_index],
-                  style: TextStyle(
-                    fontFamily: 'my',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                  ),
+        debugShowCheckedModeBanner: false,
+        builder:
+            DevicePreview.appBuilder, // Ensure DevicePreview is used properly
+        home: SafeArea(
+          child: Scaffold(
+            body: Center(child: content[_index]),
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: _index,
+              items: const [
+                BottomNavigationBarItem(
+                  label: 'Currently',
+                  icon: Icon(Iconsax.calendar_edit),
                 ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: textController,
+                BottomNavigationBarItem(
+                  label: 'Today',
+                  icon: Icon(Iconsax.calendar),
+                ),
+                BottomNavigationBarItem(
+                  label: 'Weekly',
+                  icon: Icon(Iconsax.calendar_circle),
+                ),
+              ],
+              onTap: (int newIndex) {
+                setState(() {
+                  _index = newIndex;
+                });
+              },
+            ),
+            appBar: AppBar(
+              backgroundColor: const Color.fromARGB(255, 0, 211, 158),
+              title: Padding(
+                padding: const EdgeInsets.only(top: 6, bottom: 6),
+                child: TextField(
+                  controller: text1,
                   inputFormatters: [
-                    FilteringTextInputFormatter.allow(RegExp("[a-zA-Z ]"))
+                    FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]"))
                   ],
                   style: const TextStyle(
                       fontFamily: 'my', fontWeight: FontWeight.bold),
                   decoration: const InputDecoration(
                       border: InputBorder.none,
-                      hintText: 'Search location... ex: Khouribga',
+                      hintText: 'Search location... ex:  Khouribga',
                       hintStyle: TextStyle(
                           fontFamily: 'my', fontWeight: FontWeight.w100)),
                 ),
+              ),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          String newValue = text1.text;
+                          debugPrint(newValue);
+                          content[_index] = Text(
+                            newValue.isEmpty
+                                ? 'No Location Provided'
+                                : newValue,
+                            style: const TextStyle(
+                              fontFamily: 'my',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                            ),
+                          );
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.my_location_rounded,
+                        color: Colors.black,
+                      )),
+                )
               ],
             ),
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: _index,
-            items: const [
-              BottomNavigationBarItem(
-                label: 'Currently',
-                icon: Icon(Iconsax.calendar_edit),
-              ),
-              BottomNavigationBarItem(
-                label: 'Today',
-                icon: Icon(Iconsax.calendar),
-              ),
-              BottomNavigationBarItem(
-                label: 'Weekly',
-                icon: Icon(Iconsax.calendar_circle),
-              ),
-            ],
-            onTap: (int newIndex) {
-              setState(() {
-                _index = newIndex;
-              });
-            },
-          ),
-          appBar: AppBar(
-            backgroundColor: const Color.fromARGB(255, 0, 211, 158),
-            title: Padding(
-              padding: const EdgeInsets.only(top: 6, bottom: 6),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: textController,
-                      inputFormatters: [
-                        FilteringTextInputFormatter.allow(RegExp("[a-zA-Z ]"))
-                      ],
-                      style: const TextStyle(
-                          fontFamily: 'my', fontWeight: FontWeight.bold),
-                      decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Search location... ex: Khouribga',
-                          hintStyle: TextStyle(
-                              fontFamily: 'my', fontWeight: FontWeight.w100)),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
-                        // Update the centerText based on the TextField value
-                        centerText[_index] = textController.text.isEmpty
-                            ? 'No Location Provided'
-                            : textController.text;
-                      });
-                    },
-                    icon: const Icon(
-                      Icons.my_location_rounded,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+        ));
   }
 }
