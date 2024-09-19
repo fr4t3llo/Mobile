d4c56e5edc93acee02cb221d970324aa3a041692
Saifeddine Kasmi <skasmi@e3r3p3.1337.ma>
2024-09-19 02:29:30 +0100

    update by FRATELLO

--- Modulel02/weatherAppV2_proj/lib/main.dart
+++ Modulel02/weatherAppV2_proj/lib/main.dart
@@ -1,4 +1,5 @@
 // ignore: depend_on_referenced_packages
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:device_preview_plus/device_preview_plus.dart';
@@ -44,14 +45,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _locationMessage = "Press the button to get location";
   //* to get the location for the device *//
-  Future<void> _getCities(String city) async {
-    String? apiUrl = dotenv.env['API_CITIES'] ;
-    http.Response response = await http.get(Uri.parse('$apiUrl$city&format=jsonv2'));
-    if (response.statusCode == 200) {
-      print(response.body);
-    } else {
-      return;
-    }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCities();
   }
 
   Future<void> _getCurrentLocation() async {
@@ -100,6 +98,35 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  List<dynamic> listSearch = [];
+  Future<void> _getCities() async {
+    String city = 'new york';
+    String? apiUrl = dotenv.env['API_CITIES'];
+
+    // Ensure apiUrl is not null
+    if (apiUrl == null) {
+      print('API URL is not set');
+      return;
+    }
+
+    // Perform the HTTP GET request
+    http.Response response =
+        await http.get(Uri.parse('$apiUrl$city&format=jsonv2'));
+
+    // Check if the response is successful
+    if (response.statusCode == 200) {
+      var responseBody = jsonDecode(response.body);
+
+      for (int i = 0; i < responseBody.length; i++) {
+        listSearch.add(responseBody[i]['display_name']);
+      }
+
+      print(listSearch);
+    } else {
+      print('Error: ${response.statusCode}');
+    }
+  }
+
   int _index = 0;
   String location = '';
   TextEditingController text1 = TextEditingController();
@@ -109,94 +136,151 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Consumer<MainProvider>(
-          builder: (context, value, child) => Scaffold(
-            body: PageView(
-              scrollDirection: Axis.horizontal,
-              controller: _pageController,
-              children: content,
-              onPageChanged: (value) {
-                setState(() {
-                  _index = value;
-                });
-              },
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-              backgroundColor: const Color.fromARGB(255, 0, 211, 158),
-              selectedFontSize: 15,
-              unselectedFontSize: 12,
-              currentIndex: _index,
-              items: const [
-                BottomNavigationBarItem(
-                  label: 'Currently',
-                  icon: Icon(
-                    Iconsax.calendar_edit,
-                    color: Colors.black,
-                  ),
-                ),
-                BottomNavigationBarItem(
-                  label: 'Today',
-                  icon: Icon(
-                    Iconsax.calendar,
-                    color: Colors.black,
-                  ),
-                ),
-                BottomNavigationBarItem(
-                  label: 'Weekly',
-                  icon: Icon(
-                    Iconsax.calendar_circle,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-              unselectedItemColor: Colors.black,
-              onTap: (int newIndex) {
-                setState(() {
-                  _index = newIndex;
-                  _pageController.jumpToPage(_index);
-                });
-              },
-            ),
-            appBar: AppBar(
-              backgroundColor: const Color.fromARGB(255, 0, 211, 158),
-              title: Padding(
-                padding: const EdgeInsets.only(top: 6, bottom: 6),
-                child: TextField(
-                  controller: text1,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]"))
-                  ],
-                  onChanged: (vale) {
-                    value.setCity(vale);
-                  },
-                  style: const TextStyle(
-                      fontFamily: 'my', fontWeight: FontWeight.bold),
-                  decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Search location... ex:  Khouribga',
-                      hintStyle: TextStyle(
-                          fontFamily: 'my', fontWeight: FontWeight.w100)),
+      debugShowCheckedModeBanner: false,
+      home: Consumer<MainProvider>(
+        builder: (context, value, child) => Scaffold(
+          body: PageView(
+            scrollDirection: Axis.horizontal,
+            controller: _pageController,
+            children: content,
+            onPageChanged: (value) {
+              setState(() {
+                _index = value;
+              });
+            },
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: const Color.fromARGB(255, 0, 211, 158),
+            selectedFontSize: 15,
+            unselectedFontSize: 12,
+            currentIndex: _index,
+            items: const [
+              BottomNavigationBarItem(
+                label: 'Currently',
+                icon: Icon(
+                  Iconsax.calendar_edit,
+                  color: Colors.black,
                 ),
               ),
-              actions: [
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: IconButton(
+              BottomNavigationBarItem(
+                label: 'Today',
+                icon: Icon(
+                  Iconsax.calendar,
+                  color: Colors.black,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: 'Weekly',
+                icon: Icon(
+                  Iconsax.calendar_circle,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+            unselectedItemColor: Colors.black,
+            onTap: (int newIndex) {
+              setState(() {
+                _index = newIndex;
+                _pageController.jumpToPage(_index);
+              });
+            },
+          ),
+          appBar: AppBar(
+            backgroundColor: const Color.fromARGB(255, 0, 211, 158),
+            title: Padding(
+              padding: const EdgeInsets.only(top: 6, bottom: 6),
+              child: TextField(
+                autofocus: false,
+                readOnly: true,
+                onTap: () {
+                  _getCities();
+                  showSearch(
+                      context: context, delegate: DataSearch(list: listSearch));
+                },
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]"))
+                ],
+                decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Search location... ex:  Khouribga',
+                    hintStyle: TextStyle(
+                        fontFamily: 'my', fontWeight: FontWeight.w100)),
+              ),
+            ),
+            actions: [
+              Row(
+                children: [
+                  IconButton(
                       onPressed: () async {
                         // showSearch(context: context, delegate: );
                         await _getCurrentLocation();
                         value.setLosction(_locationMessage);
-                        await _getCities('khouribga');
                       },
                       icon: const Icon(
                         Icons.my_location_rounded,
                         color: Colors.black,
                       )),
-                ),
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+  List<dynamic> list;
+  DataSearch({required this.list});
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    // action for appbar
+    return [
+      IconButton(
+          onPressed: () {
+            query = "";
+          },
+          icon: const Icon(Iconsax.close_circle))
+    ];
+    // throw UnimplementedError();
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    // icon leading
+    return (IconButton(
+        onPressed: () {
+          close(context, "test");
+        },
+        icon: const Icon(Iconsax.arrow_left)));
+    // throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // resault search
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // show when someone search for something
+    var search_list =
+        query.isEmpty ? list : list.where((p) => p.startsWith(query)).toList();
+    return ListView.builder(
+        itemCount: search_list.length,
+        itemBuilder: (context, i) {
+          return ListTile(
+            leading: const Icon(Iconsax.location),
+            title: Text(search_list[i]),
+            onTap: () {
+              query = search_list[i];
+              showResults(context);
+            },
+          );
+        });
+
+    // throw UnimplementedError();
   }
 }
