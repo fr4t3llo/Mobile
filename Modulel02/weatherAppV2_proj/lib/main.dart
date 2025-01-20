ce62ed2ef9ad35c7c54fb65c60c172b0bcdf7f91
Saifeddine Kasmi <saife.addine123@gmail.com>
2025-01-20 13:05:05 +0100

    update by FRATELLO

--- Modulel02/weatherAppV2_proj/lib/main.dart
+++ Modulel02/weatherAppV2_proj/lib/main.dart
@@ -77,8 +77,6 @@ class _MyAppState extends State<MyApp> {
         return;
       }
 
-
-      
       // Get the current position
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.high);
