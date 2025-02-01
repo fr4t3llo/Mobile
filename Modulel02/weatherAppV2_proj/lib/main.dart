c9a899d0ce3480d622f3fe6e52f4eb0358904b83
Saifeddine Kasmi <saife.addine123@gmail.com>
2025-02-01 16:47:48 +0100

    update by FRATELLO

--- Modulel02/weatherAppV2_proj/lib/main.dart
+++ Modulel02/weatherAppV2_proj/lib/main.dart
@@ -94,7 +94,6 @@ class _MyAppState extends State<MyApp> {
       });
     }
   }
-
   // ignore: non_constant_identifier_names
   Future get_city(Position p) async {
     var response = await http.Client().get(Uri.parse(
