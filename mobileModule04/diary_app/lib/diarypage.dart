1f07347fcef6aa82b063c9cc4d5ae6a77cec2992
FRATELLO <saife.addine123@gmail.com>
2025-04-08 23:01:26 +0100

    update by FRATELLO

--- mobileModule04/diary_app/lib/diarypage.dart
+++ mobileModule04/diary_app/lib/diarypage.dart
@@ -81,7 +81,6 @@ class _DiarypageState extends State<Diarypage> {
                   ],
                 ),
 
-
             
                 const SizedBox(height: 20),
                 Container(
